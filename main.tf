513d5cb9c87e3820935057b8fd68cd2734d13d37
suni <sunilkm88@gmail.com>
2024-08-07 12:23:51 +0530

    f

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
  }
 
  resource "aws_instance" "example" {
-     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+     ami           = "ami-04a81a99f5ec58529"  # Specify an appropriate AMI ID
      instance_type = "t2.micro"
  }
 
